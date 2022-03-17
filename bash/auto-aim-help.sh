c0ec9c2a2a9bcc1eec4738bc3cf24d4a30ca9261
John Jiang <jianghr1@shanghaitech.edu.cn>
2022-03-17 17:17:05 +0800

    [bash] Move bash scripts to bash folder
    Haoran Jiang 2022/03/17

---
+++ bash/auto-aim-help.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo 'use autoaim-enable  to enable  autoaim when startup'
+echo 'use autoaim-disable to disable autoaim when startup'
+echo 'use autoaim-stop    to kill    auto-aim'
+echo 'restart to apply changes'
