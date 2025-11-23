aafca7259a3672e36859e154030337e87e853e82
XuYue <AstraWhale@gmail.com>
2025-11-24 03:41:02 +0800

    let planner to generate trajectory based on robotcommand

--- planner/planner_submodule.cpp
+++ planner/planner_submodule.cpp
@@ -8,7 +8,7 @@
 namespace plan
 {
 
-    PlannerSubModule::PlannerSubModule() : SubModule(SubModuleName::PLANNER)
+    PlannerSubModule::PlannerSubModule(communicationBoard::Cboard_t& cboard) : SubModule(SubModuleName::PLANNER), cboard(cboard)
     {
         LOGM_S("[PlannerSubModule] construction completed");
     }
@@ -17,9 +17,32 @@ namespace plan
         return false;
     }
 
+    command_array_t PlannerSubModule::generate_command_array(const RobotCommand& command) {
+        command_array_t commands;
+        for (size_t i = 0; i < CMDARRAYLENGTH; ++i) {
+            commands[i] = RobotCommand{
+                command.distance,
+                command.yaw_angle+i*command.yaw_speed*float(ctl_period.count())/1e6f,
+                command.yaw_speed,
+                command.pitch_angle+i*command.pitch_speed*float(ctl_period.count())/1e6f,
+                command.pitch_speed,
+                command.target_id,
+                command.shoot_mode
+            };
+        }
+        return commands;
+    }
+
     SubModuleResult PlannerSubModule::process(std::shared_ptr<ThreadDataPack> data, 
                                      const pipeline::BasicTask* parent)
     {
+        cboard.set_robotcommand(
+            generate_command_array(data->robotcommand), 
+            data->attitude, 
+            std::chrono::microseconds(2000)
+        );
         return SubModuleResult::SUCCESS;
     }
+
+
 }
