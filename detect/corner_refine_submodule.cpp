735711ee21c41ad3f731330e3db75377d0157605
XuYue <AstraWhale@gmail.com>
2026-04-10 16:19:50 +0800

    add vislization for corner refine

--- detect/corner_refine_submodule.cpp
+++ detect/corner_refine_submodule.cpp
@@ -72,6 +72,45 @@ namespace detect
             }
         }
 
+        static std::string output_dir = "./frames/";
+
+        if (_imgshow)
+        {
+            static const cv::Scalar colors[3] = {{255, 0, 0}, {0, 0, 255}, {255, 255, 255}};
+            cv::Mat im2show = data->frame.clone();
+            for (const auto &b : data->bboxes)
+            {
+                cv::line(im2show, b.pts[0], b.pts[1], colors[2], 1);
+                cv::line(im2show, b.pts[1], b.pts[2], colors[2], 1);
+                cv::line(im2show, b.pts[2], b.pts[3], colors[2], 1);
+                cv::line(im2show, b.pts[3], b.pts[0], colors[2], 1);
+
+                const char source_tag = b.source == DetectionSource::TRADITIONAL ? 'T' : 'N';
+                cv::putText(im2show,
+                            std::string(1, source_tag),
+                            b.pts[0],
+                            cv::FONT_HERSHEY_SIMPLEX,
+                            1,
+                            colors[b.color_id]);
+            }
+
+            static bool dir_created = false;
+            if (!dir_created)
+            {
+                system(("mkdir -p " + output_dir).c_str());
+                dir_created = true;
+            }
+
+            int key = cv::waitKey(1);
+            if (key == 'p' || key == 'P') {
+                std::string filename = output_dir + "corner_refine_" + std::to_string(data->frame_counter) + ".png";
+                cv::imwrite(filename, im2show);
+            }
+
+            cv::imshow("corner_refine_submodule", im2show);
+            cv::waitKey(1);
+        }
+
         auto t_opt_end = std::chrono::steady_clock::now();
         if (_debugprint)
         {
